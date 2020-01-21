6f6b779cd43aa6b79f02e380adf5e9abec149cff
jsi-odoo <jsi@odoo.com>
2020-01-21 12:12:08 +0530

    [IMP] Added logic for find and open related Purchase order line, refactor code.

--- mrr_jsi/static/src/js/mrp_bom_rediness_report.js
+++ mrr_jsi/static/src/js/mrp_bom_rediness_report.js
@@ -8,18 +8,12 @@ var stock_report_generic = require('stock.stock_report_generic');
 var QWeb = core.qweb;
 var _t = core._t;
 
-var MrpBomReport = stock_report_generic.extend({
-    // events: {
-    //     'click .o_mrp_bom_unfoldable': '_onClickUnfold',
-    //     'click .o_mrp_bom_foldable': '_onClickFold',
-    //     'click .o_mrp_bom_action': '_onClickAction',
-    //     'click .o_mrp_show_attachment_action': '_onClickShowAttachment',
-    // },
+var MrpBomReportJsi = stock_report_generic.extend({
     events: {
         'click .o_mrp_bom_unfoldable': '_onClickUnfold',
         'click .o_mrp_bom_foldable': '_onClickFold',
         'click .o_mrp_bom_action': '_onClickAction',
-        // 'click .o_mrp_bom_action_open_pol_tree': '_onClickShowAttachment',
+        'click .o_mrp_bom_action_open_pol_tree': '_onClickShowPOL',
     },
     get_html: function() {
         var self = this;
@@ -42,24 +36,12 @@ var MrpBomReport = stock_report_generic.extend({
         var self = this;
         return this._super().then(function () {
             self.$el.html(self.data.lines);
-            // self.renderSearch();
             self.update_cp();
         });
     },
     render_html: function(event, $el, result){
-        // if (result.indexOf('mrp.document') > 0) {
-        //     if (this.$('.o_mrp_has_attachments').length === 0) {
-        //         var column = $('<th/>', {
-        //             class: 'o_mrp_has_attachments',
-        //             title: 'Files attached to the product Attachments',
-        //             text: 'Attachments',
-        //         });
-        //         this.$('table thead th:last-child').after(column);
-        //     }
-        // }
         $el.after(result);
         $(event.currentTarget).toggleClass('o_mrp_bom_foldable o_mrp_bom_unfoldable fa-caret-right fa-caret-down');
-        this._reload_report_type();
     },
     get_bom: function(event) {
       var self = this;
@@ -84,25 +66,6 @@ var MrpBomReport = stock_report_generic.extend({
               self.render_html(event, $parent, result);
           });
     },
-    get_operations: function(event) {
-      var self = this;
-      var $parent = $(event.currentTarget).closest('tr');
-      var activeID = $parent.data('bom-id');
-      var qty = $parent.data('qty');
-      var level = $parent.data('level') || 0;
-      return this._rpc({
-              model: 'report.mrr_jsi.report_bom_material_rediness',
-              method: 'get_operations',
-              args: [
-                  activeID,
-                  parseFloat(qty),
-                  level + 1
-              ]
-          })
-          .then(function (result) {
-              self.render_html(event, $parent, result);
-          });
-    },
     update_cp: function () {
         var status = {
             cp_content: {
@@ -111,54 +74,6 @@ var MrpBomReport = stock_report_generic.extend({
             },
         };
         return this.update_control_panel(status);
-    },
-    // renderSearch: function () {
-    //     this.$buttonPrint = $(QWeb.render('mrp.button'));
-    //     this.$buttonPrint.filter('.o_mrp_bom_print').on('click', this._onClickPrint.bind(this));
-    //     this.$buttonPrint.filter('.o_mrp_bom_print_unfolded').on('click', this._onClickPrint.bind(this));
-    //     this.$searchView = $(QWeb.render('mrp.report_bom_search', _.omit(this.data, 'lines')));
-    //     this.$searchView.find('.o_mrp_bom_report_qty').on('change', this._onChangeQty.bind(this));
-    //     this.$searchView.find('.o_mrp_bom_report_variants').on('change', this._onChangeVariants.bind(this));
-    //     this.$searchView.find('.o_mrp_bom_report_type').on('change', this._onChangeType.bind(this));
-    // },
-    _onClickPrint: function (ev) {
-        var childBomIDs = _.map(this.$el.find('.o_mrp_bom_foldable').closest('tr'), function (el) {
-            return $(el).data('id');
-        });
-        framework.blockUI();
-        var reportname = 'mrp.report_bom_structure?docids=' + this.given_context.active_id + '&report_type=' + this.given_context.report_type;
-        if (! $(ev.currentTarget).hasClass('o_mrp_bom_print_unfolded')) {
-            reportname += '&quantity=' + (this.given_context.searchQty || 1) +
-                          '&childs=' + JSON.stringify(childBomIDs);
-        }
-        if (this.given_context.searchVariant) {
-            reportname += '&variant=' + this.given_context.searchVariant;
-        }
-        var action = {
-            'type': 'ir.actions.report',
-            'report_type': 'qweb-pdf',
-            'report_name': reportname,                                
-            'report_file': 'mrp.report_bom_structure',
-        };
-        return this.do_action(action).then(function (){
-            framework.unblockUI();
-        });
-    },
-    _onChangeQty: function (ev) {
-        var qty = $(ev.currentTarget).val().trim();
-        if (qty) {
-            this.given_context.searchQty = parseFloat(qty);
-            this._reload();
-        }
-    },
-    _onChangeType: function (ev) {
-        var report_type = $("option:selected", $(ev.currentTarget)).data('type');
-        this.given_context.report_type = report_type;
-        this._reload_report_type();
-    },
-    _onChangeVariants: function (ev) {
-        this.given_context.searchVariant = $(ev.currentTarget).val();
-        this._reload();
     },
     _onClickUnfold: function (ev) {
         var redirect_function = $(ev.currentTarget).data('function');
@@ -178,34 +93,31 @@ var MrpBomReport = stock_report_generic.extend({
             target: 'current'
         });
     },
-    // _onClickShowAttachment: function (ev) {
-    //     ev.preventDefault();
-    //     var ids = $(ev.currentTarget).data('res-id');
-    //     return this.do_action({
-    //         name: _t('Purchase Order Lines'),
-    //         type: 'ir.actions.act_window',
-    //         res_model: $(ev.currentTarget).data('model'),
-    //         domain: [['id', 'in', ids]],
-    //         views: [[false, 'list']],
-    //         view_mode: 'list',
-    //         target: 'current',
-    //     });
-    // },
+    _onClickShowPOL: function (ev) {
+        ev.preventDefault();
+        var self = this
+        var ids = $(ev.currentTarget).data('res-id');
+        return self._rpc({
+                    model: 'ir.model.data',
+                    method: 'xmlid_to_res_id',
+                    kwargs: {xmlid: 'mrr_jsi.purchase_order_line_tree_mrr_jsi'},
+                }).then(function(view_id){
+                    self.do_action({
+                        name: _t('Purchase Order Lines'),
+                        type: 'ir.actions.act_window',
+                        res_model: $(ev.currentTarget).data('model'),
+                        domain: [['id', 'in', ids]],
+                        views: [[view_id, 'list']],
+                        view_mode: 'list',
+                        target: 'current',
+                    });
+                });
+    },
     _reload: function () {
         var self = this;
         return this.get_html().then(function () {
             self.$el.html(self.data.lines);
-            self._reload_report_type();
         });
-    },
-    _reload_report_type: function () {
-        this.$('.o_mrp_bom_cost.o_hidden, .o_mrp_prod_cost.o_hidden').toggleClass('o_hidden');
-        if (this.given_context.report_type === 'bom_structure') {
-            this.$('.o_mrp_bom_cost').toggleClass('o_hidden');
-        }
-        if (this.given_context.report_type === 'bom_cost') {
-            this.$('.o_mrp_prod_cost').toggleClass('o_hidden');
-        }
     },
     _removeLines: function ($el) {
         var self = this;
@@ -221,7 +133,7 @@ var MrpBomReport = stock_report_generic.extend({
     },
 });
 
-core.action_registry.add('mrp_bom_rediness_report', MrpBomReport);
-return MrpBomReport;
+core.action_registry.add('mrp_bom_rediness_report', MrpBomReportJsi);
+return MrpBomReportJsi;
 
 });
