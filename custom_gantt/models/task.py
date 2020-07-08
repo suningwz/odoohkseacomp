4e5a826dfd087d89be7da58b0495e1d26f8b0de9
jsi-odoo <jsi@odoo.com>
2020-07-08 17:54:39 +0530

    [ADD] Old changes.

--- custom_gantt/models/task.py
+++ custom_gantt/models/task.py
@@ -11,16 +11,10 @@ class Task(models.Model):
     task_color = fields.Integer(compute="get_task_color",store=True)
     user_name = fields.Char(compute="get_task_user_name",store=True)
 
-    # @api.depends('x_studio_effective_completion_date')
-    # def get_task_color(self):
-    # 	for rec in self:
-    # 		rec.task_color = 0 if not rec.x_studio_effective_completion_date else 3
-
-    @api.depends('name')
+    @api.depends('x_studio_effective_completion_date')
     def get_task_color(self):
         for rec in self:
-            rec.task_color = 3
-
+            rec.task_color = 0 if not rec.x_studio_effective_completion_date else 3
 
     @api.depends('user_id')
     def get_task_user_name(self):
