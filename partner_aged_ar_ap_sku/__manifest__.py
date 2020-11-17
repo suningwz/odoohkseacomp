4b13b7e17d9bc98be4cf968b78102fbd9a94d203
jsi-odoo <jsi@odoo.com>
2020-11-17 11:09:05 +0530

    [ADD] New module: Aged receivable payable-sku

---
+++ partner_aged_ar_ap_sku/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name' : 'Aged Partner Receivable and Payable report - sku',
+    'version' : '1.0',
+    'description': """
+     The purpose of this module is adding the invoice date in the aged receivable and aged payable xlsx report
+     of partner invoices.
+     """,
+    'category': 'Accounting/Accounting',
+    'depends' : ['account_reports',
+                 ],
+
+    'installable': True,
+
+}
