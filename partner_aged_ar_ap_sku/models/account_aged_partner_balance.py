478d4e2293895ad25aec18ffdf5d84e65d0c95f6
jsi-odoo <jsi@odoo.com>
2020-11-17 15:52:16 +0530

    [IMP] Date format

--- partner_aged_ar_ap_sku/models/account_aged_partner_balance.py
+++ partner_aged_ar_ap_sku/models/account_aged_partner_balance.py
@@ -56,7 +56,7 @@ class report_account_aged_partner(models.AbstractModel):
                         'caret_options': caret_type,
                         'level': 4,
                         'parent_id': 'partner_%s' % (values['partner_id'],),
-                        'columns': [{'name': v} for v in [aml.invoice_id.date_invoice,aml.journal_id.code, aml.account_id.code, self._format_aml_name(aml)]] +\
+                        'columns': [{'name': v} for v in [format_date(self.env, aml.invoice_id.date_invoice),aml.journal_id.code, aml.account_id.code, self._format_aml_name(aml)]] +\
                                    [{'name': v} for v in [line['period'] == 6-i and self.format_value(sign * line['amount']) or '' for i in range(7)]],
                         'action_context': aml.get_action_context(),
                     }
